016dc4734d5e28efe395b5b81478001766bb87f4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-01 03:07:52 +0530

    Camera: Add support for miui camera mode [1/2]
     * devices like ginkgo and some xiaomi sdm660 use miui camera mode in camera
       hal to activate certain functions in camera hal, these are enabled when
       vendor.camera.miui.apk is set to 1 based on sys.camera.miui.apk value
    
     * if this prop is set by default gcam crashes, so we must do it dynamically
    
     * xiaomi does this in stock libcameraservice but unfortunately we don't
       have stock android 12 to use prebuilt lib
    
    To enable, set TARGET_USES_MIUI_CAMERA := true in BoardConfig.
    
    Change-Id: I8d9ee4e650f3e2196546570c183c9d169b8aa335

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -35,6 +35,7 @@
 
 #include <android-base/macros.h>
 #include <android-base/parseint.h>
+#include <android-base/properties.h>
 #include <android-base/stringprintf.h>
 #include <binder/ActivityManager.h>
 #include <binder/AppOpsManager.h>
@@ -84,6 +85,7 @@ namespace {
 
 namespace android {
 
+using base::SetProperty;
 using base::StringPrintf;
 using binder::Status;
 using namespace camera3;
@@ -3539,6 +3541,17 @@ status_t CameraService::BasicClient::startCameraOps() {
     }
 
     mOpsActive = true;
+
+#ifdef USES_MIUI_CAMERA
+    // Configure miui camera mode
+    if (strcmp(String8(mClientPackageName).string(), "com.android.camera") == 0) {
+        SetProperty("sys.camera.miui.apk", "1");
+        ALOGI("Enabling miui camera mode");
+    } else {
+        SetProperty("sys.camera.miui.apk", "0");
+        ALOGI("Disabling miui camera mode");
+    }
+#endif
 
     // Transition device availability listeners from PRESENT -> NOT_AVAILABLE
     sCameraService->updateStatus(StatusInternal::NOT_AVAILABLE, mCameraIdStr);
